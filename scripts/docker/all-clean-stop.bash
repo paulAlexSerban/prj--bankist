fbca4f1799f625946799cf20bd2708a5d010eac8
paulAlexSerban <paul.alex.serban@gmail.com>
2022-12-28 14:31:27 +0200

    build(docker): added static-landing in all clean stop script

--- scripts/docker/all-clean-stop.bash
+++ scripts/docker/all-clean-stop.bash
@@ -6,6 +6,7 @@ echo " 🛑  STOP ALL 🐳  Docker and clean containers"
 docker compose --env-file ../../config.env \
   --file ../../docker/docker-compose.traefik-proxy.yml \
   --file ../../docker/docker-compose.static-app.yml \
+    --file ../../docker/docker-compose.static-landing.yml \
   down --volumes --rmi all
 docker container prune -f
 docker volume prune -f
